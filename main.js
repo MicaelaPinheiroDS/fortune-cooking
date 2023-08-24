9491bd98cf812cf569fa2dbc17d1c5ef5887f620
Micaela Pinheiro <micaela.mews@gmail.com>
2023-08-24 13:40:32 -0300

    correcting the texts

--- main.js
+++ main.js
@@ -13,47 +13,47 @@ const fortuneCookiephrases = [
 
 function startScreen () {
 
-    const h1 = new Element(main, 'h1')
-    h1.textContent = 'Qual é a sua sorte de hoje?'
+    const h1 = new Element(main, "h1")
+    h1.textContent = "what's your luck today?"
     
-    const p = new Element(main, 'p')
-    p.textContent = 'Abra o biscoito e descubra!'
+    const p = new Element(main, "p")
+    p.textContent = "open the cookie and find out!"
     
-    const img =  new Element(main, 'img', 'imgCooking')
-    img.src = './img/cooking.png'  
+    const img =  new Element(main, "img", "imgCooking")
+    img.src = "./img/cooking.png" 
     
     screenChange()
 }
 function endScreen () {
     let randomIndex = Math.floor(Math.random() * fortuneCookiephrases.length)
 
-    const h1 = new Element(main, 'h1')
-    h1.textContent = 'Aqui está a sua sorte de hoje:'
+    const h1 = new Element(main, "h1")
+    h1.textContent = "Here's your luck for today:"
 
     const div = new Element(main,'div','luckyPhrase')
-    const p = new Element(div, 'p')
+    const p = new Element(div, "p")
     p.textContent = fortuneCookiephrases[randomIndex]
 
-    const img = new Element(main, 'img', 'imgOpenedCookie')
-    img.src = './img/opened-cookie.png'
+    const img = new Element(main, "img", "imgOpenedCookie")
+    img.src = "./img/opened-cookie.png"
 
     //button reset 
-    new Element(main, 'div','buttonReset')
+    new Element(main, "div","buttonReset")
 }
 function screenChange(){
     
-        const imgCooking = document.querySelector('.imgCooking')
+        const imgCooking = document.querySelector(".imgCooking")
 
-    imgCooking.addEventListener('click', () => {
-        main.innerHTML = ''
+    imgCooking.addEventListener("click", () => {
+        main.innerHTML = ""
         endScreen()
-        const buttonReset = document.querySelector('.buttonReset')
-        buttonReset.addEventListener('click', () => {
-            main.innerHTML = ''
+        const buttonReset = document.querySelector(".buttonReset")
+        buttonReset.addEventListener("click", () => {
+            main.innerHTML = ""
             startScreen()
         })
     })  
 }
 const body = document.body
-const main = new Element(body, 'main')
+const main = new Element(body, "main")
 startScreen()
